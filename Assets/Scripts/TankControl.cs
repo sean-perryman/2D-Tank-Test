56c3967161e122874e6e6e199ece81a97f261db4
Sean Perryman <se@nperryman.com>
2015-12-14 19:37:58 -0800

    Moved from keyboard back to xbox controller, working on getting movement going.

--- Assets/Scripts/TankControl.cs
+++ Assets/Scripts/TankControl.cs
@@ -7,87 +7,39 @@ using System;
 // http://www.scriptscoop.net/t/f2885b2638eb/unity-2d-gameobject-movement-and-rotation.html
 
 public class TankControl : MonoBehaviour {
-    /*private float rotation;
     public float speed;
-    // Use this for initialization
+
+    private Transform trans;
+    private Vector3 rot;
+    private Vector3 pos;
+    float angle;
+
     void Start () {
-        rotation = 0.0f;
+        trans = transform;
+        pos = trans.position;
+        rot = trans.rotation.eulerAngles;
     }
 	
 	// Update is called once per frame
-	void Update () {       
+	void Update () {
+        angle = trans.eulerAngles.magnitude * Mathf.Deg2Rad;
+
         float lts = XCI.GetAxis(XboxAxis.LeftTrigger);
         float rts = XCI.GetAxis(XboxAxis.RightTrigger);
         
         float velocityR = rts * speed;
         float velocityL = lts * speed;
 
-        float width = 1.0f;
-
-        // "width" must be expressed in units
-        rotation += (velocityR - velocityL) / width * Time.deltaTime;
-        double newX = transform.position.x + 0.5 * (velocityR + velocityL) * Math.Cos(rotation) * Time.deltaTime;
-        double newY = transform.position.y - 0.5 * (velocityR + velocityL) * Math.Sin(rotation) * Time.deltaTime;
-        Vector3 oldTransform = new Vector3(transform.position.x, transform.position.y);
-        Vector3 newTransform = Quaternion.Euler((float)newX, (float)newY, rotation);//new Vector3((float)newX, (float)newY);
-
-        transform.Rotate(Quaternion.Euler((float)newX, (float)newY, rotation)); // = Quaternion.FromToRotation(oldTransform, newTransform);
-
-    }*/
-
-    public float speed;
-    public float rotationSpeed;
-    public float angleCorrect;
-
-    //transform
-    Transform myTrans;
-    //object position
-    Vector3 myPos;
-    //object rotation
-    Vector3 myRot;
-    //object rotation 
-    float angle;
-
-    // Use this for initialization
-    void Start()
-    {
-        myTrans = transform;
-        myPos = myTrans.position;
-        myRot = myTrans.rotation.eulerAngles;
-    }
-
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-
-        //converting the object euler angle's magnitude from to Radians    
-        angle = myTrans.eulerAngles.magnitude * Mathf.Deg2Rad + angleCorrect;
-        
-        //rotate object Right & Left
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (velocityL > 0 && velocityR > 0)
         {
-            myRot.z -= rotationSpeed;
+            pos.x += (Mathf.Cos(angle) * speed) * Time.deltaTime;
+            pos.y += (Mathf.Sin(angle) * speed) * Time.deltaTime;
         }
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            myRot.z += rotationSpeed;
-        }
-        
-        //move object Forward & Backward
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            myPos.x += (Mathf.Cos(angle) * speed) * Time.deltaTime;
-            myPos.y += (Mathf.Sin(angle) * speed) * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            myPos.x -= (Mathf.Cos(angle) * speed) * Time.deltaTime;
-            myPos.y -= (Mathf.Sin(angle) * speed) * Time.deltaTime;
-        }
-        
-        //Apply
-        myTrans.position = myPos;
-        myTrans.rotation = Quaternion.Euler(myRot);
 
+        else if (velocityL > 0) { rot.z -= velocityL; }
+        else if (velocityR > 0) { rot.z += velocityR; }
+                
+        trans.position = pos;
+        trans.rotation = Quaternion.Euler(rot);
     }
 }
