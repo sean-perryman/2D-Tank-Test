b70e79f26a624b11802845cb2217bbb49ec000f4
Sean Perryman <se@nperryman.com>
2015-12-17 21:09:43 -0800

    Need to revert.

--- Assets/Scripts/SmoothCamera2D.cs
+++ Assets/Scripts/SmoothCamera2D.cs
@@ -6,7 +6,7 @@ using System.Collections;
 
 public class SmoothCamera2D : MonoBehaviour
 {
-
+    //public GameObject controlSurface;
     public float dampTime = 0.15f;
     private Vector3 velocity = Vector3.zero;
     public Transform target;
@@ -20,6 +20,7 @@ public class SmoothCamera2D : MonoBehaviour
             Vector3 delta = target.position - GetComponent<Camera>().ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); //(new Vector3(0.5, 0.5, point.z));
             Vector3 destination = transform.position + delta;
             transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+            //controlSurface.transform.position = new Vector3(transform.position.x, transform.position.y - 3.8f, transform.position.x);
         }
 
     }
