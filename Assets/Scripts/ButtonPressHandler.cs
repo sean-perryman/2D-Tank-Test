68e64cdd17ee6cb2c93b67dad284f5ac1d789fea
Sean Perryman <se@nperryman.com>
2015-12-26 23:06:56 -0800

    Worked on intro scene, got the button press worked out.

---
+++ Assets/Scripts/ButtonPressHandler.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonPressHandler : MonoBehaviour {
+
+    public void btn_QuitGame() {
+        Application.Quit();
+    }
+
+    public void btn_RestartGame() { }
+
+    public void btn_Credits() { }
+
+    public void btn_Instructions() { }
+
+    public void btn_StartGame() { }
+}
